7b346d1be40393821c306bf0110478fc6839970e
Matthew Emmett <matthew.emmett@amd.com>
2021-05-01 09:59:42 -0600

    Remove duplicate build entries.

--- library/src/device/kernel-generator.py
+++ library/src/device/kernel-generator.py
@@ -399,7 +399,7 @@ def list_old_generated_kernels(patterns=None,
     for patt in patterns:
         for prec in precisions:
             gen += generated_kernels[f'kernels_launch_{patt}_{prec}']
-    return gen
+    return list(set(gen))
 
 
 def list_generated_kernels(kernels):
